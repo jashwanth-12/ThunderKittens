30c69f793aa23969a7449752d4231736633436d6
jashwanth-12 <jashwanth.p12@gmail.com>
2026-08-06 01:33:46 +0000

    level 3 working

--- kernels/practice/level_03.cu
+++ kernels/practice/level_03.cu
@@ -19,13 +19,9 @@
 __global__ void kernel(__nv_bfloat16* A, __nv_bfloat16* B, __nv_bfloat16* C, int N) {
     __shared__ __nv_bfloat16 ATile[16][16];
     __shared__ __nv_bfloat16 BTile[16][16];
-    __shared__ __nv_bfloat16 CTile[16][16];
 
     int row = blockIdx.x * blockDim.x + threadIdx.x;
     int col = blockIdx.y * blockDim.y + threadIdx.y;
-
-    CTile[threadIdx.x][threadIdx.y] = 0;
-    __syncthreads();
 
     float totalSum = 0.0;
     for (int i=0;i<N/16;i++) {
@@ -41,13 +37,13 @@ __global__ void kernel(__nv_bfloat16* A, __nv_bfloat16* B, __nv_bfloat16* C, int
 
         float sum = 0.0;
         for(int j=0;j<16;j++) {
-            sum += __bfloat162float(ATile[threadIdx.x][threadIdx.y] * BTile[threadIdx.x][threadIdx.y]);
+            sum += __bfloat162float(ATile[threadIdx.x][j] * BTile[j][threadIdx.y]);
         }
-        CTile[threadIdx.x][threadIdx.y] += sum;
+        totalSum += sum;
+        __syncthreads();
     }
-    __syncthreads();
 
-    C[row * N + col] = __float2bfloat16(CTile[threadIdx.x][threadIdx.y]);
+    C[row * N + col] = __float2bfloat16(totalSum);
 }
 
 void matmul(__nv_bfloat16* A, __nv_bfloat16* B, __nv_bfloat16* C, int N) {
