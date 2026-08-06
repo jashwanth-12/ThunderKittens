45b68817d82a3c732d4e930a6fc4f16f21782046
jashwanth-12 <jashwanth.p12@gmail.com>
2026-08-06 01:26:10 +0000

    level 3 shared , a shot

--- kernels/practice/level_03.cu
+++ kernels/practice/level_03.cu
@@ -24,23 +24,27 @@ __global__ void kernel(__nv_bfloat16* A, __nv_bfloat16* B, __nv_bfloat16* C, int
     int row = blockIdx.x * blockDim.x + threadIdx.x;
     int col = blockIdx.y * blockDim.y + threadIdx.y;
 
-    // Load A and B tiles
-    int aRow = blockIdx.x * blockDim.x + threadIdx.x;
-    int aCol = blockIdx.z * blockDim.y + threadIdx.y;
-    int bRow = blockIdx.z * blockDim.x + threadIdx.x;
-    int bCol = blockIdx.y * blockDim.y + threadIdx.y;
-
-    ATile[threadIdx.x][threadIdx.y] = A[aRow * N + aCol];
-    BTile[threadIdx.x][threadIdx.y] = B[bRow * N + bCol];
     CTile[threadIdx.x][threadIdx.y] = 0;
-
     __syncthreads();
 
-    float sum = 0.0;
-    for (int i = 0; i < 16; i++) {
-        sum += __bfloat162float(ATile[threadIdx.x][i] * BTile[i][threadIdx.y]);
+    float totalSum = 0.0;
+    for (int i=0;i<N/16;i++) {
+        // Load ith tile in A and B into shared mem
+        int aRow = blockIdx.x * blockDim.x + threadIdx.x;
+        int aCol = i * blockDim.y + threadIdx.y;
+        int bRow = i * blockDim.x + threadIdx.x;
+        int bCol = blockIdx.y * blockDim.y + threadIdx.y;
+        ATile[threadIdx.x][threadIdx.y] = A[aRow * N + aCol];
+        BTile[threadIdx.x][threadIdx.y] = B[bRow * N + bCol];
+
+        __syncthreads();
+
+        float sum = 0.0;
+        for(int j=0;j<16;j++) {
+            sum += __bfloat162float(ATile[threadIdx.x][threadIdx.y] * BTile[threadIdx.x][threadIdx.y]);
+        }
+        CTile[threadIdx.x][threadIdx.y] += sum;
     }
-    CTile[threadIdx.x][threadIdx.y] += sum;
     __syncthreads();
 
     C[row * N + col] = __float2bfloat16(CTile[threadIdx.x][threadIdx.y]);
@@ -49,7 +53,7 @@ __global__ void kernel(__nv_bfloat16* A, __nv_bfloat16* B, __nv_bfloat16* C, int
 void matmul(__nv_bfloat16* A, __nv_bfloat16* B, __nv_bfloat16* C, int N) {
     int T = 16;
     dim3 threads(T, T);
-    dim3 blocks(N / T, N /T, N/T);
+    dim3 blocks(N / T, N /T);
     kernel<<<blocks, threads>>>(A, B, C, N);
 }
 
